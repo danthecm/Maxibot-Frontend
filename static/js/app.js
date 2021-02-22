7fe820ba15bc801603a9d3555123bd46e5389215
CM <etchiedaniel1@gmail.com>
2021-02-23 00:16:10 +0100

    Started

---
+++ static/js/app.js
@@ -0,0 +1,40 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+const modalBtn = document.querySelector("#modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal-overlay");
+const my_Form = document.getElementById("form")
+const modalToggle = () => modal.classList.toggle("open-modal");
+const btn = document.querySelector("#run");
+const run = document.querySelector("#running")
+
+console.log("starting")
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    attrib = run.getAttribute("hidden")
+    console.log(attrib)
+    run.removeAttribute("hidden");
+    console.log(my_Form);
+    my_Form.submit();
+    console.log("done");
+    modal.classList.remove("open-modal")
+    let inputs = document.querySelectorAll("input")
+    console.log("done")
+    inputs.forEach(element => {
+        element.value = ""
+    });
+    
+})
+
+modalBtn.addEventListener("click", () => {
+    my_modal = document.querySelector("#modal")
+    console.log(my_modal)
+    my_modal.removeAttribute("hidden")
+    console.log("done")
+})
+
+closeBtn.addEventListener("click", modalToggle)
+
