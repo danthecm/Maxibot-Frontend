023b6065665fb7821108527feb19ced2ed44d96f
CM <52335952+danieletchie@users.noreply.github.com>
2021-03-01 23:10:33 +0100

    Update av.py

--- av.py
+++ av.py
@@ -5,37 +5,37 @@ secret_key = "LqLDBStDa1BPACEQ1Dryml1zQTWS8YMmnsvkLDoUhPNpjPHtoptaBPrbDTFgQHCL"
 def get_average(api_key, secret_key):
     try:
         client = Client(api_key, secret_key)
-    except Exception as e:
+        all_buy_price = []
+        all_buy_qty = []
+
+        def buyTrades(trade):
+            if trade["side"] == "BUY" and trade["status"] == "FILLED":
+                all_buy_price.append(float(trade["price"]))
+                all_buy_qty.append(float(trade["origQty"]))
+                return True
+
+        current_price = client.get_symbol_ticker(symbol="BNBGBP")
+        first_coin = client.get_asset_balance(asset="BNB")
+        second_coin = client.get_asset_balance(asset="GBP")
+        orders = client.get_all_orders(symbol='BNBGBP')
+
+        buy_orders = filter(buyTrades, orders)
+        buy_orders = list(buy_orders)
+        print(f"You've made a total of {len(orders)} orders")
+        total_price = 0
+        total_qty = 0
+        for price in all_buy_price:
+            total_price += price
+        for qty in all_buy_qty:
+            total_qty += qty
+        average = total_price/total_qty
+        print(f"The total buy amount is {total_price}")
+        print(f"The total buy quantity is {total_qty}")
+        print(f"The average price is {average}")
+        print(f"The current price is {current_price}")
+        print(f"{first_coin}")
+        print(f"{second_coin}")
+        return average
+    except:
         print(e)
         return "Your API KEY is invalide"
-    all_buy_price = []
-    all_buy_qty = []
-
-    def buyTrades(trade):
-        if trade["side"] == "BUY" and trade["status"] == "FILLED":
-            all_buy_price.append(float(trade["price"]))
-            all_buy_qty.append(float(trade["origQty"]))
-            return True
-
-    current_price = client.get_symbol_ticker(symbol="BNBGBP")
-    first_coin = client.get_asset_balance(asset="BNB")
-    second_coin = client.get_asset_balance(asset="GBP")
-    orders = client.get_all_orders(symbol='BNBGBP')
-
-    buy_orders = filter(buyTrades, orders)
-    buy_orders = list(buy_orders)
-    print(f"You've made a total of {len(orders)} orders")
-    total_price = 0
-    total_qty = 0
-    for price in all_buy_price:
-        total_price += price
-    for qty in all_buy_qty:
-        total_qty += qty
-    average = total_price/total_qty
-    print(f"The total buy amount is {total_price}")
-    print(f"The total buy quantity is {total_qty}")
-    print(f"The average price is {average}")
-    print(f"The current price is {current_price}")
-    print(f"{first_coin}")
-    print(f"{second_coin}")
-    return average
