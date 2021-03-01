205221b36fb16222cac050883175e32c8e223fe8
CM <52335952+danieletchie@users.noreply.github.com>
2021-03-01 23:49:08 +0100

    Update av.py

--- av.py
+++ av.py
@@ -37,7 +37,7 @@ def get_average(api_key, secret_key):
         print(f"{first_coin}")
         print(f"{second_coin}")
         print(f"Average from binance is {average_p}")
-        return average_p
+        return average_p["Price"]
     except Exception as e:
         print(e)
         return "Your API KEY is invalid"
