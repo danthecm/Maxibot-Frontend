6cb415c498b1042504667ca04d0363ef0745c5c6
CM <etchiedaniel1@gmail.com>
2021-03-12 06:44:11 +0100

    checking celery

--- app.py
+++ app.py
@@ -19,16 +19,15 @@ app.config['CELERY_RESULT_BACKEND'] = 'db+mysql://admin:maxitest@maxitest.cepigw
 app.config['CELERY_BROKER_URL'] = "amqps://wwbcioqn:Wrs1zKw7legb6ISqBKNbRBkXmII4Y6Sf@woodpecker.rmq.cloudamqp.com/wwbcioqn"
 
 
-celery = make_celery(app)
+# app.config["CELERYBEAT_SCHEDULE"] = {
+#     'add-every-30-seconds': {
+#         'task': 'my_test.my_task',
+#         'schedule': 10,
+#         'args': ("woooooooo")
+#     },
+# }
 
-celery.conf.beat_schedule = {
-    'add-every-30-seconds': {
-        'task': 'my_test.my_task',
-        'schedule': 30.0,
-        'args': ("woooooooo")
-    },
-}
-celery.conf.timezone = 'UTC'
+celery = make_celery(app)
 
 
 @app.route("/")
@@ -38,8 +37,8 @@ def index():
 
 @app.route("/celery")
 def check():
-    my_task.delay("ooooo boy eh")
-
+    me = my_task.delay("This is me starting")
+    print("working on the task")
     return "I sent a request"
 
 @celery.task(name="my_test.my_task")
