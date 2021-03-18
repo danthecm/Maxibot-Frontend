ecc911e909826e883b5a2f3b7ef28298122dfe0c
CM <etchiedaniel1@gmail.com>
2021-03-18 02:36:57 +0100

    added current price to trade table

--- db.py
+++ db.py
@@ -163,7 +163,7 @@ def email_exist(email):
 #     print(" YOu've entered the wrong email")
 # else:
 #     print(f"WELCOME {me[1]} your email address is {me[2]} and your phone number is {me[3]} all other information are secret bro ")
-createTrades()
+# createTrades()
 # delete()
 # me = result(20)
 # answer = pickle.loads(me["result"])
