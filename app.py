c84451fa2a68be681c485721322a9d394a8f0479
CM <etchiedaniel1@gmail.com>
2021-05-02 23:26:31 +0100

    working on pagination

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ def dashboard(page_num=1):
         user = user.decode("UTF-8")
         user = ast.literal_eval(user)
         print(user_req.status_code)
-        data = [session["user_id"], str(page_num)]
+        data = [str(session["user_id"]), str(page_num)]
         trade_req = requests.get(f"{maxi_backend}my_trades", data=data)
         print(f"the trade request return a status of {trade_req.status_code}")
         trades = trade_req.content
